5e675f0ad331dd2352e729ff849724a92558f551
JJuDD17 <34658105+JJuDD17@users.noreply.github.com>
2020-02-01 11:27:05 +0300

    Add files via upload

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    post_text = models.CharField(max_length=500)
+    post_image = models.ImageField('image', blank=True)
+    pub_date = models.DateTimeField('date posted')
+
+    def __str__(self):
+        return self.post_text
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
 from django.core.handlers.wsgi import WSGIRequest
+from django.template import loader
+from .models import Question, Post
 
-a = 0
-def index(request):
-    global a
-    a += 1
-    with open('./static/index.html') as file:
-        return HttpResponse(file.read())
+from string import ascii_letters
+from random import randint, choice
+
+ascii_letters += ',.:; '
+def index(request: WSGIRequest):
+    latest_question_list = Question.objects.order_by('-pub_date')
+    template = loader.get_template('polls\index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+        #'comments': [''.join([choice(ascii_letters) for letter in range(randint(10, 80))]) for comment in range(randint(2, 15))]
+    }
+    return HttpResponse(template.render(context, request))
+
+def posts(request: WSGIRequest):
+    #posts_list = Post.objects.order_by('-pub_date')
+    posts_list = Post.objects.filter(post_text='12345')
+    template = loader.get_template('polls\posts.html')
+    context = {
+        'posts_list': posts_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def work(request: WSGIRequest):
     text = request.GET.get('data')
